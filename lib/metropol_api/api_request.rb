20e6e3c0a586eec35e1d954a8b30a19cb71946a6
StlMaris123 <stellamarisnjage@gmail.com>
2018-04-17 11:25:13 +0300

    Test an dimplement the api headers

---
+++ lib/metropol_api/api_request.rb
@@ -0,0 +1,82 @@
+require 'uri'
+require 'rest-client'
+require 'json'
+require 'digest'
+require 'metropol_api/default_configurations'
+require 'metropol_api/report_reason'
+require 'metropol_api/identity_type'
+
+module MetropolApi
+  class ApiRequest
+
+    include MetropolApi::DefaultConfigurations
+    include MetropolApi::ReportReason
+    include MetropolApi::IdentityType
+
+    def initialize(private_key:, public_key:, path:, port:, api_version:, payload: {})
+      @private_key = private_key
+      @public_key = public_key
+      @path = path
+      @port = port
+      @api_version = api_version
+      @payload = payload
+    end
+
+    def api_headers
+      headers = default_headers
+      headers['X-METROPOL-REST-API-KEY'] = @public_key
+      headers['X-METROPOL-REST-API-TIMESTAMP'] = rest_api_hash(api_timestamp)
+      headers['X-METROPOL-REST-API-HASH'] = api_timestamp
+      headers
+    end
+
+    def api_timestamp
+      utc_time = Time.now.utc
+      utc_time.strftime('%Y%m%d%H%M%S%6N')
+    end
+
+    def rest_api_hash(api_timestamp)
+      json_object = JSON.generate(@payload)
+      expected_key = @private_key + json_object + @public_key + api_timestamp
+      Digest::SHA256.hexdigest(expected_key.encode('UTF-8'))
+    end
+
+    def api_url
+      URI::HTTPS.build(
+        host: HOST,
+        port: port,
+        path: "/#{api_version}/#{@path}"
+
+      ).to_s
+    end
+
+    def post
+      response = RestClient.post(api_url, json_payload, api_headers)
+      JSON.parse response.body
+    end
+
+    def method_missing(method_name, *args, &block)
+      if valid_identity_types? method_name
+        @payload[:identity_number] = args.first
+        @payload[:identity_type] = valid_identity_types(method_name)
+        return post
+      end
+      super
+    end
+
+    private
+
+    def port
+      @port || DEFAULT_PORT
+    end
+
+    def api_version
+      @api_version || DEFAULT_API_VERSION
+    end
+
+    def json_payload
+      @payload.to_json
+    end
+  end
+
+end
---
+++ test/metropol_api/api_request_test.rb
@@ -0,0 +1,41 @@
+require "test_helper"
+require './lib/metropol_api/api_request'
+
+describe MetropolApi::ApiRequest do 
+
+  describe 'Generating Api URL' do
+    it 'should be in the correct format' do
+      @request = MetropolApi::ApiRequest.new(public_key: 'public', private_key: 'private', path: 'testing', port: 3000, api_version: 'v2')
+      url = 'https://api.metropol.co.ke:3000/v2/testing'
+      assert_equal @request.api_url, url
+    end
+  end
+
+  describe 'Generating Api headers' do
+    it 'returns the required keys' do
+      @request = MetropolApi::ApiRequest.new(public_key: 'public', private_key: 'private', path: 'testing', port: 5555, api_version: 'v2_1')
+      headers = @request.api_headers
+      refute_nil headers['X-METROPOL-REST-API-KEY']
+      refute_nil headers['X-METROPOL-REST-API-TIMESTAMP']
+      refute_nil headers['X-METROPOL-REST-API-HASH']
+    end
+  end
+
+  describe 'api time stamp' do
+    it 'has 20 characters' do
+      @request = MetropolApi::ApiRequest.new(public_key: 'public', private_key: 'private', path: 'testing', port: 5555, api_version: 'v2_1')
+      assert 20, @request.api_timestamp.length
+    end
+  end
+
+  describe 'X-METROPOL-REST-API-HASH' do
+    it "returns a sha256 hexdigest length string" do
+      @request = MetropolApi::ApiRequest.new(public_key: 'public', private_key: 'private', path: 'testing', port: 5555, api_version: 'v2_1')
+      time_stamp = @request.api_timestamp
+      rest_api_hash = @request.rest_api_hash(time_stamp)
+      assert 64,  rest_api_hash.length
+      assert String, rest_api_hash.class
+    end
+  end
+
+end
